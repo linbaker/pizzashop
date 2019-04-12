92ebd848bcafc922a3ca19656f75e227504756ab
Lindsey Baker <lindsey.baker0@gmail.com>
2019-04-12 13:20:04 -0700

    Add inputs to Pizza and Order

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,31 @@ function Order() {
 };
 
 //Create Pizza Order
-function Pizza() {
+function Pizza(size, cheese, toppings) {
   this.size = size,
   this.cheese = cheese,
-  this.toppings = []
+  this.toppings = toppings
 }
 
+//Push pizza object to order array
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza);
+};
 
 //User Interface Logic
 $(function() {
+  var pizza;
   $(".pizzaForm").submit(function(event) {
     event.preventDefault();
     var size = $("input:radio[name=size]:checked").val();
     var cheese = $("input:radio[name=cheese]:checked").val();
-    var toppings = $("input:checkbox[name=toppings]:checked").val();
-    console.log(size, cheese, toppings);
+    var toppings = [];
+      $.each($("input[name='toppings']:checked"), function(){
+        toppings.push($(this).val());
+      });
+    var pizza = new Pizza(size, cheese, toppings);
+    var order = new Order(pizza);
+    order.addPizza(pizza);
+    console.log(order);
   });
 });
