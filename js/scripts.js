5412f572a2d2f2f0546e05f01547a8ecdadf0fb9
Lindsey Baker <lindsey.baker0@gmail.com>
2019-04-12 09:39:44 -0700

    Add Order and Pizza constructor object

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Business Logic
+
+//Create and Hold Pizza Order
+function Order() {
+  this.pizzas = [],
+  this.currentId =0
+};
+
+//Create Pizza Order
+function Pizza() {
+  this.size = size,
+  this.cheese = cheese,
+  this.toppings = []
+}
+
+
+// //User Interface Logic
+// $(function() {
+//   $(".pizzaForm").submit(function(event) {
+//     event.preventDefault();
+//     var size = $()
+//   });
+// });
